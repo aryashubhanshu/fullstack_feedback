7a9fc42f4941b4397a42722f3b29270bb9f204e3
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-06-07 14:05:05 +0530

    Fixed Bugs

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -49,7 +49,7 @@ export async function POST(request: Request) {
         verifyCodeExpires: expiryDate,
         isVerified: false,
         isAcceptingMessages: true,
-        messages: true,
+        messages: [],
       });
 
       await newUser.save();
