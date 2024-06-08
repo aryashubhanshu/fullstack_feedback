7f050fd234c71d5c7cfbf5454fc9ede180fab5e3
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-06-08 15:27:42 +0530

    Added Message Card UI using ShadCN

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,16 +1,7 @@
-import Navbar from "@/components/Navbar";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+  return <div>{children}</div>;
 }
