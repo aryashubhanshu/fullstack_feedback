57e346388866c636cb19784b24cd3700a18bf87c
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-06-08 10:50:16 +0530

    Signup Page UI using ShadCN

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,21 +1,5 @@
-"use client";
+const Page = () => {
+  return <div>Page</div>;
+};
 
-import { useSession, signIn, signOut } from "next-auth/react";
-
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  );
-}
+export default Page;
